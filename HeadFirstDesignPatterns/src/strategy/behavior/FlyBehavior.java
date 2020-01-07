ee4268545bde7e70c8c80a0b74d9b14a5528c910
machine-human <dhwltn02@gmail.com>
2020-01-07 18:52:35 +0900

    Behavior 인터페이스 추가 (나는 행동과 소리내는 행동)

---
+++ HeadFirstDesignPatterns/src/strategy/behavior/FlyBehavior.java
@@ -0,0 +1,6 @@
+package strategy.behavior;
+
+// 나는 행동의 인터페이스
+public interface FlyBehavior {
+	public void fly();
+}
---
+++ HeadFirstDesignPatterns/src/strategy/behavior/QuackBehavior.java
@@ -0,0 +1,6 @@
+package strategy.behavior;
+
+// 소리내는 행동의 인터페이스
+public interface QuackBehavior {
+	public void quack();
+}
