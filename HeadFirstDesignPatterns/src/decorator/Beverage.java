6611756ad8668693b4051e029cec36d61d16377d
machine-human <dhwltn02@gmail.com>
2020-01-13 13:28:00 +0900

    decorator - Beverage 추상 클래스 추가 (CondimentDecorator 추상클래스를 확장하면 CondimentDecorator에서 Beverage를 확장)

---
+++ HeadFirstDesignPatterns/src/decorator/Beverage.java
@@ -0,0 +1,11 @@
+package decorator;
+
+public abstract class Beverage {
+	protected String description = "Empty";
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public abstract int cost();
+}
