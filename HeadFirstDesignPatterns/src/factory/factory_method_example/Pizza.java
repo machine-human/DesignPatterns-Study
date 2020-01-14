ce157cfbb26a75bd6987fa7aa3fae1166e6e82c5
machine-human <dhwltn02@gmail.com>
2020-01-14 19:18:18 +0900

    factory_method - Pizza 추상 클래스 추가

---
+++ HeadFirstDesignPatterns/src/factory/factory_method_example/Pizza.java
@@ -0,0 +1,38 @@
+package factory.factory_method_example;
+
+import java.util.ArrayList;
+
+public abstract class Pizza {
+	protected String name;
+	protected String dough;
+	protected String sauce;
+	protected ArrayList<String> toppings = new ArrayList<>();
+
+	public void prepare() {
+		System.out.println("Preparing : " + name);
+		System.out.println("Tossing dough...");
+		System.out.println("Adding source");
+		System.out.println("Adding toppings");
+		
+		for (String topping : toppings) {
+			System.out.println("\ttopping : " + topping);
+		}
+	}
+
+	public void bake() {
+		System.out.println("Bake for 25 minutes at 350");
+	}
+
+	public void cut() {
+		System.out.println("Cutting the pizza into diagonal slices");
+	}
+
+	public void box() {
+		System.out.println("Place pizza in official PizzaStore box");
+	}
+
+	public String getname() {
+		return this.name;
+	}
+
+}
